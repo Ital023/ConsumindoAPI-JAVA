3f543cf69a23be0d1d6397a9b092c527dd089d2c
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-04-22 20:43:54 -0300

    Solicitando dados da api e mostrando o arquivo JSON na tela

--- src/principal.java
+++ src/principal.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class principal {
-    public static void main(String[] args) {
-        System.out.println("oi");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String apiKey = "57b012a6";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Insira o nome do filme: ");
+        String filme = sc.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t="+filme+"&apikey="+apiKey;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+
     }
 }
